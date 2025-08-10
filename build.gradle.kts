f140930e84fd6ea967fe9703deb95ff5ae1da98f
kvv1968 <kvv1968@mail.ru>
2025-08-10 20:03:23 +0300

    lib  10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,53 +1,44 @@
 plugins {
     java
-    id("java")
     id("maven-publish")
 }
-
-publishing {
-    publications {
-        // Define a publication named 'mavenJava' for Java/Kotlin JVM projects
-        create<MavenPublication>("mavenJava") {
-            // 'from(components["java"])' or 'from(components["kotlin"])'
-            // depending on the component you want to publish
-            from(components["java"]) // Or components["kotlin"] for Kotlin JVM
-
-            // Optional: Override groupId, artifactId, or version for this specific publication
-            // groupId = "com.example.override"
-            // artifactId = "my-custom-library"
-            // version = "1.0.1"
-
-            // Optional: Add additional artifacts like sources or javadoc JARs
-            artifact(tasks.jar) {
-                classifier = "sources" // Example for a sources JAR
-            }
-            artifact(tasks.jar) {
-                classifier = "javadoc" // Example for a javadoc JAR
-            }
-        }
-    }
-    repositories {
-        // Define repositories where your artifacts will be published
-        mavenLocal() // Publish to your local Maven repository (for testing)
-
-        maven {
-            name = "lib-custom-dto" // A descriptive name for your repository
-            url = uri("https://github.com/uteam-top/lib-custom-dto") // URL of your remote repository
-            credentials {
-                username = "Uteam-TOP"
-                password = "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"
-            }
-        }
-    }
-}
-
 
 group = "ru.fvds.cdss13.lib"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
+    withSourcesJar()
+    withJavadocJar()
 }
+
+repositories {
+    mavenCentral()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifactId = project.name
+            version = project.version.toString()
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Uteam-TOP/lib-custom-dto")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
+
 dependencies {
     compileOnly("org.projectlombok:lombok:1.18.24")
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.18.2")
@@ -59,8 +50,5 @@ dependencies {
 
 
 
-repositories {
-    mavenCentral()
-}
 
 
