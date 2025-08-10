3f93afa94cd1baed6085a53240a5a73bfd60ae77
kvv1968 <kvv1968@mail.ru>
2025-08-10 19:06:38 +0300

    lib  8

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ publishing {
 
         maven {
             name = "lib-custom-dto" // A descriptive name for your repository
-            url = uri("https://github.com/Uteam-TOP/lib-custom-dto") // URL of your remote repository
+            url = uri("https://github.com/uteam-top/lib-custom-dto") // URL of your remote repository
             credentials {
                 username = System.getenv("GITHUB_USERNAME") // Get username from environment variable
                 password = System.getenv("GITHUB_PASSWORD")
