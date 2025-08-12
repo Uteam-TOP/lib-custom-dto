53210871645c26e17052afd46c6a9fe677684235
kvv1968 <kvv1968@mail.ru>
2025-08-13 00:10:13 +0300

    lib  32

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "ru.fvds.cdss13.lib"
-version = "0.0.22-SNAPSHOT"
+version = "0.0.23-SNAPSHOT"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
---
+++ src/main/java/ru/fvds/cdss13/lib/dto/ApiResponse.java
@@ -0,0 +1,38 @@
+package ru.fvds.cdss13.lib.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ApiResponse<Dto> {
+
+    private boolean success;
+    private String message;
+    private Dto data;  // Обобщенный тип для полезной нагрузки
+
+    // Геттеры и сеттеры (обязательны для десериализации JSON)
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Dto getData() {
+        return data;
+    }
+
+    public void setData(Dto data) {
+        this.data = data;
+    }
+
+    public ApiResponse() {
+    }
+}
