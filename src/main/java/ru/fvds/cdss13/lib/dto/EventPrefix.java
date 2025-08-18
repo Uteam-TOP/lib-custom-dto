f2f09ec4d698e2beb0e4dac79f5393d01b9415b7
kvv1968 <kvv1968@mail.ru>
2025-08-18 14:42:16 +0300

    lib  67

--- src/main/java/ru/fvds/cdss13/lib/dto/EventPrefix.java
+++ src/main/java/ru/fvds/cdss13/lib/dto/EventPrefix.java
@@ -1,15 +1,6 @@
 package ru.fvds.cdss13.lib.dto;
 
 public enum EventPrefix {
-    CREATE_,
-    UPDATE_,
-    DELETE_,
-    ALL_,
-    ALL_FILTER_,
-    FILTER_,
-    GET_BY_,
-    REQUEST_,
-    RESPONSE_,
-    BAN_,
-    UN_BAN_
+    _REQUEST,
+    _RESPONSE
 }
