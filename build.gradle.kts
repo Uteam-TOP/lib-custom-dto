c691b9e2de6f9efcd3437b50ee6a5758af6f2437
kvv1968 <kvv1968@mail.ru>
2025-08-18 18:16:40 +0300

    lib  71

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "ru.fvds.cdss13.lib"
-version = "0.0.47"
+version = "0.0.48"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
--- src/main/java/ru/fvds/cdss13/lib/dto/ad/RequestPageableAllDto.java
+++ src/main/java/ru/fvds/cdss13/lib/dto/ad/RequestPageableAllDto.java
@@ -3,18 +3,18 @@ package ru.fvds.cdss13.lib.dto.ad;
 
 import org.springframework.data.domain.Pageable;
 
-public class RequestAdsAllDto<Dto> {
+public class RequestPageableAllDto<Dto> {
 
     private Dto dto;
 
     private Pageable pageable;
 
-    public RequestAdsAllDto(Dto dto, Pageable pageable) {
+    public RequestPageableAllDto(Dto dto, Pageable pageable) {
         this.dto = dto;
         this.pageable = pageable;
     }
 
-    public RequestAdsAllDto() {
+    public RequestPageableAllDto() {
     }
 
     public Dto getDto() {
@@ -38,7 +38,7 @@ public class RequestAdsAllDto<Dto> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
-        RequestAdsAllDto<?> that = (RequestAdsAllDto<?>) o;
+        RequestPageableAllDto<?> that = (RequestPageableAllDto<?>) o;
 
         if (!dto.equals(that.dto)) return false;
         return pageable.equals(that.pageable);
