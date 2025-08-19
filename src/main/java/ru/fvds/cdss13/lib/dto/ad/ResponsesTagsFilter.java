f2d0bfbddd98d99e3598d4854316b0f5df026d2e
kvv1968 <kvv1968@mail.ru>
2025-08-19 10:11:42 +0300

    lib  81

---
+++ src/main/java/ru/fvds/cdss13/lib/dto/ad/ResponsesTagsFilter.java
@@ -0,0 +1,55 @@
+package ru.fvds.cdss13.lib.dto.ad;
+
+
+import ru.fvds.cdss13.lib.dto.tag.TagDto;
+import ru.fvds.cdss13.lib.dto.tag.TagType;
+
+import java.util.List;
+
+public class ResponsesTagsFilter {
+
+    private List<TagType> types;
+
+    private List<TagDto> dtos;
+
+    public ResponsesTagsFilter(List<TagType> types) {
+        this.types = types;
+    }
+
+    public ResponsesTagsFilter() {
+    }
+
+    public List<TagType> getTypes() {
+        return types;
+    }
+
+    public void setTypes(List<TagType> types) {
+        this.types = types;
+    }
+
+    public List<TagDto> getDtos() {
+        return dtos;
+    }
+
+    public void setDtos(List<TagDto> dtos) {
+        this.dtos = dtos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ResponsesTagsFilter that = (ResponsesTagsFilter) o;
+
+        if (!types.equals(that.types)) return false;
+        return dtos != null ? dtos.equals(that.dtos) : that.dtos == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = types.hashCode();
+        result = 31 * result + (dtos != null ? dtos.hashCode() : 0);
+        return result;
+    }
+}
